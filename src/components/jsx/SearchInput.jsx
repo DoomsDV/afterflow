fbfba765e1ecd088f157f0595ceea18c44d7ea77
daniel <dvillasantivergara1232@hotmail.com>
2023-11-02 18:43:24 -0300

    refact: se elimino la lista de coicidencias
    La lista no era necesaria

--- src/components/jsx/SearchInput.jsx
+++ src/components/jsx/SearchInput.jsx
@@ -1,33 +1,44 @@
 import { urlAPI } from "../../static/ts/vars";
 
 export default function SearchInput() {
+    let inputText = '';
+    let listWords = [];
     async function getPosts(e) {
+            
         if (e.target.value !== '') {
-            let coincidences = [];
-            const request = await fetch(urlAPI + `/search/posts/${e.target.value}/`);
-            const posts = await request.json();
+            inputText = e.target.value
+            listWords.push(inputText)
+            
+            if (listWords.length > 0) {
 
-            posts.map(post => {
-                if (coincidences.includes(post) === false) {
-                    coincidences.push(post);
+                /* Comparamos si la longitud de la palabra actual que se escribio 
+                es mayor a la última palabra que escribio el usuario y que esta guardado en 
+                listWords. Si se cumple se hace la petición si no significa que la palabra 
+                actual tiene una longitud menor por lo tanto significa que se elimino 
+                un caracter y no realiza la petición.*/ 
+
+                if (inputText.length >= listWords[listWords.length-1].length) {
+                    const request = await fetch(urlAPI + `/search/posts/${e.target.value}/`);
+                    const posts = await request.json();
+
+                    let postsPanel = document.getElementById('posts');
+                    postsPanel.innerHTML = ''
+
+                    posts.map((post) => {
+                        postsPanel.innerHTML += `
+                            <a href=${'/blog/' + post.slug} title=${post.title} class='block text-xs my-1 p-2 rounded-lg line-clamp-1 hover:bg-slate-50'>${post.title}</a>
+                        `
+                    })
+                    postsPanel.classList.remove('hidden')
                 }
-            })
-
-            let postsPanel = document.getElementById('posts');
-            postsPanel.innerHTML = ''
-
-            coincidences.map((post) => {
-                postsPanel.innerHTML += `
-                    <a href=${'/blog/' + post.slug} class='block text-xs my-1 p-2 rounded-lg line-clamp-1 hover:bg-slate-50'>${post.title}</a>
-                `
-            })
-            postsPanel.classList.remove('hidden')
+            }
         }
     }
     function removePostPanel() {
         const postsPanel = document.getElementById('posts');
         postsPanel.innerHTML = ''
         postsPanel.classList.add('hidden')
+        listWords = [];
     }
     return (
         <>
