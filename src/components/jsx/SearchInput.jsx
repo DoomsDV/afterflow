38f97e0b33da40fb1ade1e44befc08df704a70d4
daniel <dvillasantivergara1232@hotmail.com>
2023-11-02 17:43:54 -0300

    style: se mejoro el diseño responsive del sitio

--- src/components/jsx/SearchInput.jsx
+++ src/components/jsx/SearchInput.jsx
@@ -18,7 +18,7 @@ export default function SearchInput() {
 
             coincidences.map((post) => {
                 postsPanel.innerHTML += `
-                    <a href=${'/blog/' + post.slug} class='block'>${post.title}</a>
+                    <a href=${'/blog/' + post.slug} class='block text-xs my-1 p-2 rounded-lg line-clamp-1 hover:bg-slate-50'>${post.title}</a>
                 `
             })
             postsPanel.classList.remove('hidden')
@@ -36,7 +36,7 @@ export default function SearchInput() {
                     <label for="search" class="hidden">Buscar artículo</label>
                     <input onFocus={getPosts} onBlur={removePostPanel} onInput={getPosts} type="text" placeholder="Buscar artículos" id="search" class="bg-zinc-100/80 text-sm placeholder:text-zinc-400 placeholder:text-sm px-3 h-9 rounded-lg w-full outline-[1px] outline-slate-100" />
                 </form>
-                <div id={"posts"} className={"hidden rounded-lg shadow px-3 py-2 w-80 h-auto absolute right-[-2rem] overflow-y-auto bg-white"}></div>
+                <div id={"posts"} className={"hidden rounded-lg shadow px-3 py-2 w-80 max-h-[200px] absolute right-[-2rem] overflow-y-auto bg-white"}></div>
             </div>
         </>
     )
