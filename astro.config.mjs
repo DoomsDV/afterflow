1c3223dc67a41aeaef06cc417a12a33c3b79bbb6
daniel <dvillasantivergara1232@hotmail.com>
2023-11-03 14:27:31 -0300

    se agrego mdx al proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), mdx()],
   server: {
     headers: {
-      'Access-Control-Allow-Origin': 'no-cors',
-    },
+      'Access-Control-Allow-Origin': 'no-cors'
+    }
   }
 });
