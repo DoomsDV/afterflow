55f218cb82d5729e9d8b2e85ff15026d4cd0ff05
daniel <dvillasantivergara1232@hotmail.com>
2023-11-09 09:36:09 -0300

    feat: ahora las categorias se obtienen de manera local

--- src/components/jsx/ButtonsSidePanel.jsx
+++ src/components/jsx/ButtonsSidePanel.jsx
@@ -15,14 +15,6 @@ export function ButtonShowSideBar() {
     )
 }
 
-export function ButtonShowMenuCategories({parent}) {
-    return (
-        <ul class="text-gray-700" className={parent === 'sideBar' ? "pl-6 mt-3" : ""}>
-            <li><a href="/categoria/astro-js" title={"Astro JS"} aria-label={"Categoría astro JS"} className={"block dark:hover:bg-zinc-800 dark:hover:text-zinc-200 p-2 rounded-lg hover:bg-zinc-100"}>Astro JS</a></li>
-        </ul>
-    )
-}
-
 export function BlockDiv() {
     function hideSideBar(event) {
         let sideBar = document.getElementById('sideBar');
