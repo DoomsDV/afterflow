3381cf972dd4e5198f8d317d61b8ebbcf47932b1
daniel <dvillasantivergara1232@hotmail.com>
2023-11-09 09:22:33 -0300

    feat: ahora la publicacion destacada es obtenida desde la fuente local

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,26 @@
-import {z, defineCollection } from "astro:content";
+import {z, defineCollection, reference } from "astro:content";
+
+const tags = defineCollection({
+    type: 'data',
+    schema: z.object({
+        name: z.string(),
+        slug: z.string(),
+    })
+})
 
 const blogCollection = defineCollection({
-    type: 'content',
+    type: 'data',
     schema: z.object({
         title: z.string(),
+        slug: z.string(),
         createdOn: z.date(),
         image: z.string().url(),
+        summary: z.string(),
+        tags: reference('tags'),
     })
 });
 
 export const collection = {
     'blog': blogCollection,
+    'tags': tags
 }
