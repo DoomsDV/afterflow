574167da08fa0ca2378d65a2b5e1eb5dd181c356
daniel <dvillasantivergara1232@hotmail.com>
2023-11-14 11:21:49 -0300

    Se agrego el archivo para las reglas de formateo

--- src/components/jsx/ButtonsSidePanel.jsx
+++ src/components/jsx/ButtonsSidePanel.jsx
@@ -1,28 +1,38 @@
 export function ButtonShowSideBar() {
-    let sideBar = document.getElementById('sideBar');
-    let bloqueo = document.getElementById('bloqueo');
+	let sideBar = document.getElementById("sideBar");
+	let bloqueo = document.getElementById("bloqueo");
 
-    function showSideBar() {
-        sideBar.style.right = '0px';
-        bloqueo.classList.replace('hidden', 'block');
-    }
-    return (
-        <>
-            <button onClick={showSideBar} className={"mdl:block hidden"} title={"Menú"}>
-                <svg class="swap-off fill-current w-7 h-7 text-zinc-800 dark:text-slate-200" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
-            </button>
-        </>
-    )
+	function showSideBar() {
+		sideBar.style.right = "0px";
+		bloqueo.classList.replace("hidden", "block");
+	}
+	return (
+		<>
+			<button
+				onClick={showSideBar}
+				className={"hidden mdl:block"}
+				title={"Menú"}>
+				<svg
+					class="swap-off h-7 w-7 fill-current text-zinc-800 dark:text-slate-200"
+					viewBox="0 0 512 512">
+					<path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+				</svg>
+			</button>
+		</>
+	);
 }
 
 export function BlockDiv() {
-    function hideSideBar(event) {
-        let sideBar = document.getElementById('sideBar');
-        sideBar.style.right = '-20rem';
-        event.target.classList.replace('block', 'hidden');
-    }
+	function hideSideBar(event) {
+		let sideBar = document.getElementById("sideBar");
+		sideBar.style.right = "-20rem";
+		event.target.classList.replace("block", "hidden");
+	}
 
-    return (
-        <div id="bloqueo" onClick={hideSideBar} class="w-full h-full fixed top-0 z-[100] md:visible invisible bg-black/30 hidden"></div>
-    )
+	return (
+		<div
+			id="bloqueo"
+			onClick={hideSideBar}
+			class="invisible fixed top-0 z-[100] hidden h-full w-full bg-black/30 md:visible"></div>
+	);
 }
--- src/static/ts/vars.ts
+++ src/static/ts/vars.ts
@@ -1,2 +1,2 @@
-export const urlAPI = "https://afterflow-api.onrender.com/api"
-export const urlAPIHome = "https://afterflow-api.onrender.com"
+export const urlAPI = "https://afterflow-api.onrender.com/api";
+export const urlAPIHome = "https://afterflow-api.onrender.com";
